2bfdc35f4b0b652c547ca04197d887e26c42d5f8
Morax997 <71130183+Morax997@users.noreply.github.com>
2022-12-17 21:25:05 +0100

    firstCommit

---
+++ src/apis/config.js
@@ -0,0 +1,2 @@
+export const API_KEY = "b9fe7cfafedd694d34902039255ea7e8";
+export const API_BASE_URL = 'http://api.openweathermap.org/';
---
+++ src/components/CitySelector.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import {Row, Col, FormControl, Button} from 'react-bootstrap';
+import {API_BASE_URL, API_KEY} from "../apis/config";
+
+
+const CitySelector = () => {
+  const [city, setCity] = useState('');
+  const [results, setResults] = useState(null)
+  const onSearch = () => {
+    fetch(`${API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
+      .then((response) => response.json())
+      .then((results) => setResults(results));
+  }
+  const onKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      onSearch();
+    }
+  }
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <h1>Search your city</h1>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={4} className="text-center">
+          <FormControl
+            placeholder="Enter city"
+            onChange={(event) => setCity(event.target.value)}
+            value={city}
+            onKeyDown={onKeyDown}
+          />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Button onClick={onSearch}>Check Weather</Button>
+        </Col>
+      </Row>
+    </>
+  );
+
+};
+
+export default CitySelector;
